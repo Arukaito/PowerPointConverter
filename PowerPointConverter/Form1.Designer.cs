f370df0ff90950d3758a718fa69b431d875ff9d8
Alkaid Darooga <alkaid_dlarosa@hotmail.com>
2017-09-26 14:04:33 -0500

    Primera implementacion del PowerPoint Interop

--- PowerPointConverter/Form1.Designer.cs
+++ PowerPointConverter/Form1.Designer.cs
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Button_Convertir = new System.Windows.Forms.Button();
+            this.Button_Seleccion = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Button_Convertir);
+            this.groupBox1.Controls.Add(this.Button_Seleccion);
+            this.groupBox1.Location = new System.Drawing.Point(2, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(270, 198);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // Button_Convertir
+            // 
+            this.Button_Convertir.Location = new System.Drawing.Point(10, 48);
+            this.Button_Convertir.Name = "Button_Convertir";
+            this.Button_Convertir.Size = new System.Drawing.Size(251, 23);
+            this.Button_Convertir.TabIndex = 1;
+            this.Button_Convertir.Text = "Convertir";
+            this.Button_Convertir.UseVisualStyleBackColor = true;
+            this.Button_Convertir.Click += new System.EventHandler(this.ButtonConvertirClick);
+            // 
+            // Button_Seleccion
+            // 
+            this.Button_Seleccion.Location = new System.Drawing.Point(10, 19);
+            this.Button_Seleccion.Name = "Button_Seleccion";
+            this.Button_Seleccion.Size = new System.Drawing.Size(251, 23);
+            this.Button_Seleccion.TabIndex = 0;
+            this.Button_Seleccion.Text = "Seleccionar Folder";
+            this.Button_Seleccion.UseVisualStyleBackColor = true;
+            this.Button_Seleccion.Click += new System.EventHandler(this.ButtonSeleccionClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(275, 262);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Power Point Converter";
+            this.Load += new System.EventHandler(this.Form1Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Button_Convertir;
+        private System.Windows.Forms.Button Button_Seleccion;
     }
 }
 
--- PowerPointConverter/Form1.cs
+++ PowerPointConverter/Form1.cs
@@ -1,20 +1,145 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Form1.cs" company="Alkaworks">
+//   Copyright Alkaworks
+// </copyright>
+// <summary>
+//   Defines the Form1 type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace PowerPointConverter
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing.Drawing2D;
+    using System.IO;
+    using System.Windows.Forms;
+    using Microsoft.Office.Core;
+    using pptApplication = Microsoft.Office.Interop.PowerPoint;
+    
+    /// <summary>
+    /// The form 1.
+    /// </summary>
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// </summary>
         public Form1()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// The form 1_ load.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        private void Form1Load(object sender, EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// The button click.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        private void ButtonSeleccionClick(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+            if (fb.ShowDialog() == DialogResult.OK)
+            {
+                VariablesGlobales.Directorio = fb.SelectedPath;
+            }
+            var directory = new DirectoryInfo(VariablesGlobales.Directorio);
+            var files = directory.GetFiles("*.pptx");
+        
+            foreach (var file in files)
+            {
+               Archivos ar = new Archivos();
+                file.FullName = ar.File[0];
+            }
+        }
+
+        /// <summary>
+        /// The button to start converting the files.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        private void ButtonConvertirClick(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbe = new FolderBrowserDialog();
+            if (fbe.ShowDialog() == DialogResult.OK)
+            {
+                VariablesGlobales.DirectorioExpo = fbe.SelectedPath;
+            }
+
+
+           pptApplication.Application pptp = new pptApplication.Application();
+            pptApplication.Presentation ppps = pptp.Presentations.Open(
+                @"\\ramfile01\LGroups\Engineering\Cambios de Ingenieria\Software Developement\ECR Enforcer.pptx",
+                MsoTriState.msoFalse,
+                MsoTriState.msoFalse,
+                MsoTriState.msoFalse);
+
+            var slideCount = ppps.Slides.Count;
+            var diff = 0;
+
+            foreach (Microsoft.Office.Interop.PowerPoint.Slide pptSlides in ppps.Slides)
+            {
+                diff++;
+                pptSlides.Export(VariablesGlobales.DirectorioExpo + "\\" + diff + " de " + slideCount + ".jpg", "jpg", 3840, 2160);
+            }
+
+            MessageBox.Show("Convertido con Exito!");
+        }
+
+        /// <summary>
+        /// The Public Variables.
+        /// </summary>
+        public class VariablesGlobales
+        {
+            /// <summary>
+            /// Gets or sets the directory.
+            /// </summary>
+            public static string Directorio { get; set; }
+
+            /// <summary>
+            /// Gets or sets the directory expo.
+            /// </summary>
+            public static string DirectorioExpo { get; set; }
+        }
+
+        /// <summary>
+        /// The archive.
+        /// </summary>
+        public class Archivos
+        {
+            /// <summary>
+            /// The file.
+            /// </summary>
+            private string[] file;
+
+            /// <summary>
+            /// Gets or sets the file.
+            /// </summary>
+            public string[] File
+            {
+                get => this.file;
+                set => this.file = value;
+            }
         }
     }
 }
